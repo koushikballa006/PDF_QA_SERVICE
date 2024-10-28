5bf0ff584a92359ae8835cd79a7b0f274e706ed6
koushikballa006 <koushikballa06@gmail.com>
2024-10-28 16:06:54 +0530

    main.py CORS setup

--- app/main.py
+++ app/main.py
@@ -3,3 +3,20 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import documents
 from app.api.websockets import qa
 from app.core.config import get_settings
+
+settings = get_settings()
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    version=settings.VERSION,
+)
+
+#CORS Config
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
